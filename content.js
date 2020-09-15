04da595e162316da30e6e8eea645be0a0bc90bb8
luubaminh <luubaminh17042000@gmail.com>
2020-09-15 13:40:44 +0700

    Hoan tat code tu dong lam task

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
-
 function timer(ms) {
     return new Promise(res => setTimeout(res, ms));
 }
-var task = document.getElementById('dtasktitle').innerHTML;
-var timeWait = 30000;
+//var task = document.getElementById('dtasktitle').innerHTML;
+
+var timeWait = 6 * 60000;
+
 function vocabulary() {
     var clickAudio = document.getElementsByClassName('fa-play-circle');
     var i;
@@ -15,7 +16,7 @@ function vocabulary() {
         //4
         document.getElementsByClassName("btn-info")[1].click();
 
-        setTimeout(function () {
+        setTimeout(function() {
             window.location.reload();
         }, 5000);
     }
@@ -35,48 +36,49 @@ function mcq() {
     }
 
     load();
-    setTimeout(function () {
+    setTimeout(function() {
         window.location.reload();
     }, 4000 * click.length);
 }
+
 function checkRadio(radio) {
     var i;
     for (i = 0; i < radio.length; i++) {
         radio[i].checked = true;
     }
-    setTimeout(function () {
+    setTimeout(function() {
         document.getElementsByClassName("btn-info")[1].click();
-    }, 33000);
+    }, timeWait + 3000);
 
-    setTimeout(function () {
+    setTimeout(function() {
         document.getElementsByClassName("btn-danger")[0].click();
-    }, 36000);
+    }, timeWait + 6000);
 
     var results = [];
-    setTimeout(function () {
+    setTimeout(function() {
         for (i = 0; i < radio.length; i++) {
             if (radio[i].checked) results[i] = true;
             else results[i] = false;
         }
-    }, 37000);
-    setTimeout(function () {
+    }, timeWait + 7000);
+    setTimeout(function() {
         var lamLai = document.getElementsByClassName("btn-primary");
         if (lamLai) lamLai[0].click();
-    }, 38000);
-    setTimeout(function () {
+    }, timeWait + 8000);
+    setTimeout(function() {
         for (i = 0; i < radio.length; i++) {
             radio[i].checked = results[i];
         }
-    }, 40000);
+    }, timeWait + 10000);
     //4
-    setTimeout(function () {
+    setTimeout(function() {
         document.getElementsByClassName("btn-info")[1].click();
-    }, 43000);
+    }, timeWait + 13000);
 
 
-    setTimeout(function () {
+    setTimeout(function() {
         window.location.reload();
-    }, 45000);
+    }, timeWait + 15000);
 }
 
 function checkWrite() {
@@ -87,37 +89,38 @@ function checkWrite() {
     for (i = 0; i < writeAns.length; i++) {
         writeAns[i].value = "LMinh";
     }
-    setTimeout(function () {
+    setTimeout(function() {
         document.getElementsByClassName("btn-info")[1].click();
-    }, 33000);
+    }, timeWait + 3000);
     //2
-    setTimeout(function () {
+    setTimeout(function() {
         document.getElementsByClassName("btn-danger")[0].click();
-    }, 36000);
-    setTimeout(function () {
+    }, timeWait + 6000);
+    setTimeout(function() {
         for (i = 0; i < writeAns.length; i++) {
             answer[i] = writeAns[i].value;
         }
-    }, 39000);
+    }, timeWait + 9000);
     //3
-    setTimeout(function () {
+    setTimeout(function() {
         var lamLai = document.getElementsByClassName("btn-primary");
-        if (lamLai.length>0) lamLai[0].click();
-    }, 40000);
+        if (lamLai.length > 0) lamLai[0].click();
+    }, timeWait + 10000);
 
-    setTimeout(function () {
+    setTimeout(function() {
         for (i = 0; i < writeAns.length; i++) {
             writeAns[i].value = answer[i];
         }
-    }, 43000);
+    }, timeWait + 13000);
     //4
-    setTimeout(function () {
+    setTimeout(function() {
         document.getElementsByClassName("btn-info")[1].click();
-    }, 45000);
-    setTimeout(function () {
+    }, timeWait + 15000);
+    setTimeout(function() {
         window.location.reload();
-    }, 48000);
+    }, timeWait + 18000);
 }
+var task = document.getElementById('dtasktitle').innerHTML;
 if (task.indexOf('vocabulary') != -1) {
     vocabulary();
 } else if (task.indexOf('mcq') != -1) {
@@ -127,14 +130,14 @@ if (task.indexOf('vocabulary') != -1) {
     var checkAudio = false;
     var audio = document.getElementsByClassName('aud-btn play');
     var writeAns = document.getElementsByClassName('danw dinline');
-    if (audio.length>0) {
-        setTimeout(function () { audio[0].click(); }, 30000);
-        checkAudio = true;
+    if (audio.length > 0) {
+        audio[0].click();
+        setTimeout(function() { checkAudio = true; }, timeWait);
     }
     //setTimeout(function () {}, timeWait);
     var radio = document.getElementsByClassName('deck');
     //if(document.body.contains(radio))
-    if (radio.length>0) {
+    if (radio.length > 0) {
         checkRadio(radio);
     } else {
         checkWrite();
